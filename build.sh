c6a7ea320737647dc631d70f7156395ed2221cea
sahilmansuri777 <your-email@example.com>
2025-07-15 19:50:20 +0530

    s

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # पायथन वर्जन सुनिश्चित करें
-pyenv install 3.10.13 -s
-pyenv local 3.10.13
+pyenv install 3.13.4 -s
+pyenv local 3.13.4
 
 # वर्चुअल एनवायरनमेंट बनाएं
 python -m venv .venv
